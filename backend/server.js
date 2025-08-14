19ae81aebef7e3eda163e75002a74576cd9fa2ba
HariShankarLB <lbhari007shankar@gmail.com>
2025-08-14 10:22:28 +0530

    database connection done

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 // import express from "express";   -> "type":"module"
 const express=require("express");
 const noteRoutes = require("./routes/noteRoutes");
+const { connectDB } = require("./config/db")
+const dotenv = require("dotenv")
+
+dotenv.config();
 
 const app = express()
 
 app.use("/api/notes", noteRoutes);
 
-
+connectDB();
 
 app.listen(5001, ()=>{
     console.log("server is running");
 })
+
+
